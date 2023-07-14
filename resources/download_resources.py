a627f069431be6bc85745fd1764fb1bdb70de0d6
Arne Hilbig <666arnehilbig@gmail.com>
2023-07-14 12:56:10 +0200

    Refactoring file

--- resources/download_resources.py
+++ resources/download_resources.py
@@ -1,15 +1,30 @@
+# Imports
+from typing import List
 from webdav4.client import Client
+from pathlib import Path
 
+
+# Constants
 WEBDAV_URL = "https://cloud.ngitl.dev/remote.php/dav/files/raai_download/"
-
-client = Client(WEBDAV_URL, auth=("raai_download", "mJpyehF5M5ehWJQKBsxOcW3ctn2tm4Ip"))
-
-results = client.ls("/")
-
-for result in results:
-    print(result["name"])
+CURRENT_DIR = Path(__file__).parent
 
 
-client.download_file("/Nextcloud.png", "foo.png")
+def main() -> None:
+    client = Client(WEBDAV_URL, auth=("raai_download", "mJpyehF5M5ehWJQKBsxOcW3ctn2tm4Ip"))
+    ls_results: List[str] = client.ls("/large_files", False)
+    for file in ls_results:
+        file_name = file[::-1]
+        file_name = file_name[: file_name.find("/")][::-1]
+        dir_for_file = CURRENT_DIR / file_name
+
+        print("Downloading: " + file_name)
+        client.download_file(file, dir_for_file)
+        if Path.exists(dir_for_file):
+            print("File downloaded successfully")
+        else:
+            print("File could not be downloaded")
+        print("\n")
 
 
+if __name__ == "__main__":
+    main()
