c79126e12dd9dc7aab0ad157a5b456d494ff542a
Arne Hilbig <666arnehilbig@gmail.com>
2023-08-07 14:54:56 +0200

    Fixed import issues; Small tweaks

--- example/video_file_source_example.py
+++ example/video_file_source_example.py
@@ -2,22 +2,23 @@
 import sys
 from pathlib import Path
 
-from vehicle_tracking.image_sources import VideoFileSource
-from vehicle_tracking.tracker import VehicleTracker
+sys.path.append(str(Path(__file__).parent.parent / "vehicle_tracking"))
 
-FILE_DIR = Path(__file__).parent
+from image_sources import VideoFileSource
+from tracker import VehicleTracker
+
 
 # Constants
-VIDEO_FILE_SOURCE_FILEPATH = FILE_DIR.parent / "resources/example_video.avi"
+FILE_DIR = Path(__file__).parent
+VIDEO_FILE_SOURCE_FILEPATH = FILE_DIR.parent / "resources/test_video_1.h265"
 
 
 if __name__ == "__main__":
-
     if not VIDEO_FILE_SOURCE_FILEPATH.exists():
-        print("Please run downloadscirpt in ...")
+        print("Please run download script in 'resources/download_resources.py'")
         sys.exit(-1)
 
-    source = VideoFileSource(VIDEO_FILE_SOURCE_FILEPATH, frame_rate=30)
+    source = VideoFileSource(VIDEO_FILE_SOURCE_FILEPATH, frame_rate=60)
     tracker = VehicleTracker(source)
     while True:
         tracker.step()
