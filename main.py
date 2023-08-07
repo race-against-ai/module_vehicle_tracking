3f381856b39f57db32afcc9c7b201defb6db8aee
Arne Hilbig <666arnehilbig@gmail.com>
2023-08-07 14:28:58 +0200

    Added source example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# Copyright (C) 2022 NG:ITL
-from vehicle_tracking.image_sources import VideoFileSource, CameraStreamSource
+# Copyright (C) 2023, NG:ITL
+from vehicle_tracking.image_sources import CameraStreamSource, VideoFileSource
 from vehicle_tracking.tracker import VehicleTracker
+from pathlib import Path
 
 
 # Constants
@@ -9,6 +10,7 @@ FRAME_RECEIVE_LINK = "ipc:///tmp/RAAI/camera_frame.ipc"
 
 if __name__ == "__main__":
     source = CameraStreamSource(FRAME_RECEIVE_LINK)
+    # source = VideoFileSource(Path("resources/test_video_1.h265"), 1000)
     tracker = VehicleTracker(source)
     while True:
         tracker.step()
