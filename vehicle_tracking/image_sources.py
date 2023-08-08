dea82e73f01ed9f828a0b46f6bae9c082a8f8611
Arne Hilbig <666arnehilbig@gmail.com>
2023-08-08 11:13:04 +0200

    Docstrings; Copyright; Bug fix

--- vehicle_tracking/image_sources.py
+++ vehicle_tracking/image_sources.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2023, NG:ITL
 from pathlib import Path
 from time import sleep
 from pynng import Sub0
@@ -10,21 +11,21 @@ class VideoFileSource:
         """An image source which reads images from a file.
 
         Args:
-            video_file_filepath (str): The path of the video to be read from.
-            frame_rate (int): The frame rate of the video.
+            `video_file_filepath (Path)`: The path of the video to be read from.
+            `frame_rate (int)`: The frame rate of the video.
         """
         self.__time_to_sleep = 1 / frame_rate
-        self.__video_capture = cv2.VideoCapture(video_file_filepath)
+        self.__video_capture = cv2.VideoCapture(str(video_file_filepath))
         self.frame_size = self.read_new_frame().shape
 
     def read_new_frame(self) -> np.ndarray:
         """This function reads a frame and returns the image.
 
         Raises:
-            IndexError: If the file is not found or if it is empty.
+            `IndexError`: If the file is not found or if it is empty.
 
         Returns:
-            np.ndarray: The frame to be returned.
+            `np.ndarray`: The frame to be returned.
         """
         success, frame = self.__video_capture.read()
         if not success:
@@ -38,9 +39,9 @@ class CameraStreamSource:
         """An image source which reads images from the pynng camera stream.
 
         Args:
-            address (str): The pynng address to connect to the camera stream.
+            `address (str)`: The pynng address to connect to the camera stream.
         """
-        self.frame_size = (480, 640, 3)
+        self.frame_size = (990, 1332, 3)
         self.__frame_receiver = Sub0()
         self.__frame_receiver.subscribe("")
         self.__frame_receiver.dial(address)
@@ -49,7 +50,7 @@ class CameraStreamSource:
         """This function reads a frame and returns the image.
 
         Returns:
-            np.ndarray: The frame to be returned.
+            `np.ndarray`: The frame to be returned.
         """
         frame_bytes = self.__frame_receiver.recv()
         frame = np.frombuffer(frame_bytes, np.uint8)
