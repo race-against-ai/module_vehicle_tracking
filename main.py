ab841ce8bb434f3e265008ef83b3f0b5445f0052
Arne Hilbig <666arnehilbig@gmail.com>
2023-08-15 11:43:47 +0200

    Fixed relative imports

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # Copyright (C) 2023, NG:ITL
-from sys import path as sys_path
-from pathlib import Path
-
-sys_path.append(str(Path(__file__).parent / "vehicle_tracking"))
-
 from vehicle_tracking.image_sources import CameraStreamSource, VideoFileSource
 from vehicle_tracking.tracker import VehicleTracker
+from pathlib import Path
 
 
 # Constants
@@ -13,8 +9,8 @@ FRAME_RECEIVE_LINK = "ipc:///tmp/RAAI/camera_frame.ipc"
 
 
 if __name__ == "__main__":
-    source = CameraStreamSource(FRAME_RECEIVE_LINK)
-    # source = VideoFileSource(Path("resources/test_video_1.h265"), 1000)
+    # source = CameraStreamSource(FRAME_RECEIVE_LINK)
+    source = VideoFileSource(Path("resources/test_video_1.h265"), 1000)
     tracker = VehicleTracker(source)
     while True:
         tracker.step()
