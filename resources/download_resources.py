fe702c39c72a94bab1f74eb63def9a3b7f3bdbea
Arne Hilbig <666arnehilbig@gmail.com>
2023-10-05 15:06:14 +0200

    Added tmp frolder from tests vscode folder downloaded files

--- resources/download_resources.py
+++ resources/download_resources.py
@@ -1,25 +1,34 @@
+"""A script to download resources from a webdav server."""
 # Copyright (C) 2023, NG:ITL
-from webdav4.client import Client
-from typing import Any
 from pathlib import Path
+from typing import Any
 from json import load
+
+from webdav4.client import Client
+
+
+# Constants
+CURRENT_DIR = Path(__file__).parent
 
 
 def main() -> None:
-    current_dir = Path(__file__).parent
-    with open(current_dir.parent / "vehicle_tracking_config.json", "r") as f:
-        config: dict[str, Any] = load(f)["resource_downloader"]
+    """Download resources from a webdav server."""
+    config_file_path = CURRENT_DIR.parent / "vehicle_tracking_config.json"
+    with open(config_file_path, "r", encoding="utf-8") as config_file:
+        config: dict[str, Any] = load(config_file)["resource_downloader"]
 
     webdav_url = config["url"]
     client_name = config["client_name"]
     client_password = config["client_password"]
     client = Client(webdav_url, auth=(client_name, client_password))
 
-    ls_results: list[str] = client.ls("/large_files", False)
+    ls_results = client.ls("/large_files", False)
     for file in ls_results:
+        if not isinstance(file, str):
+            continue
         file_name = file[::-1]
         file_name = file_name[: file_name.find("/")][::-1]
-        dir_for_file = current_dir / file_name
+        dir_for_file = CURRENT_DIR / file_name
 
         print("Downloading: " + file_name)
         client.download_file(file, dir_for_file)
