29588942901c81af11ce5db0404ecbf5f648c7aa
Arne Hilbig <666arnehilbig@gmail.com>
2024-01-18 10:02:14 +0100

    Refactor and moved file

--- utils/download_resources.py
+++ utils/download_resources.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
+"""Downloads the resources from the webdav server."""
 # Copyright (C) 2023, NG:ITL
-from webdav4.client import Client
-from typing import Any
+
 from pathlib import Path
+from typing import Any
 from json import load
+
+from webdav4.client import Client
+
+
+CURRENT_DIR = Path(__file__).parent
 
 
 def main() -> None:
-    current_dir = Path(__file__).parent
-    with open(current_dir.parent / "vehicle_tracking_config.json", "r") as f:
+    """Downloads the resources from the webdav server."""
+    with open(CURRENT_DIR.parent / "vehicle_tracking_config.json", "r", encoding="utf-8") as f:
         config: dict[str, Any] = load(f)["resource_downloader"]
 
     webdav_url = config["url"]
@@ -15,11 +22,14 @@ def main() -> None:
     client_password = config["client_password"]
     client = Client(webdav_url, auth=(client_name, client_password))
 
-    ls_results: list[str] = client.ls("/large_files", False)
+    ls_results = client.ls("/large_files", False)
+    if not isinstance(ls_results, list):
+        raise TypeError("The ls_results variable should be a list.")
+
     for file in ls_results:
         file_name = file[::-1]
         file_name = file_name[: file_name.find("/")][::-1]
-        dir_for_file = current_dir / file_name
+        dir_for_file = CURRENT_DIR.parent / "resources" / file_name
 
         print("Downloading: " + file_name)
         client.download_file(file, dir_for_file)
